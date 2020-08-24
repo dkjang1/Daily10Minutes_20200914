ccf01dc2b002fe6108e267a8f87aaf185001c5a7
JangDongKuk <dkjang@naver.com>
2020-08-24 22:20:19 +0900

    서버로그인 호출/응답처리 작업

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -9,6 +9,11 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+
+        //idEdt.text = ""
+        //pwEdt.text = ""
     }
 
     override fun setupEvents() {
@@ -19,7 +24,7 @@ class MainActivity : BaseActivity() {
             val inputId =  idEdt.text.toString()
             val inputPw = pwEdt.text.toString()
 
-            //서버에 보내기
+            //6.서버에 보내기
 
 
         }
---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
@@ -0,0 +1,45 @@
+package kr.co.tjoeun.daily10minutes_20200824.utils
+
+import okhttp3.*
+import java.io.IOException
+
+//6
+class ServerUtil {
+
+    //변수+함수 객체를 이용하지않고 클래스 기능으로 활용
+    companion object {
+
+        val BASE_URL = "http://15.164.153.174" //호스트주소
+
+        fun postRequestLogin(id : String, pw: String){
+
+            val client = OkHttpClient() //클라언트동작
+            val urlStr = "${BASE_URL}/user" //주소완성
+            //파라미터(POST/PUT/PATCH) 값
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
+            //파라미터(POST/PUT/PATCH) 값 보내기
+            val request = Request.Builder()
+                .url(urlStr)
+                .post(formData)
+                .build()
+
+            client.newCall(request).enqueue(object :Callback{
+                //서버연결실패할경우
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+                //서버연결성공할경우
+                override fun onResponse(call: Call, response: Response) {
+
+                }
+
+            })
+
+        }
+
+    }
+
+}
