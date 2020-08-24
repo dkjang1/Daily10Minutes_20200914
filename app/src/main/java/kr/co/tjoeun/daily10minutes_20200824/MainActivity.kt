8232e7be482fadf34ed44cd9b565fc0ab3cee09e
JangDongKuk <dkjang@naver.com>
2020-08-24 21:44:48 +0900

    로그인버튼 이벤트- 입력값 서버보내기전

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 //1
 class MainActivity : BaseActivity() {
@@ -11,6 +12,17 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        //5
+        loginBtn.setOnClickListener {
+
+            val inputId =  idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            //서버에 보내기
+
+
+        }
 
     }
 
