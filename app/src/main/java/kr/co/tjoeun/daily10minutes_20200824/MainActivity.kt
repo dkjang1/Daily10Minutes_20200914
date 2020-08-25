ee5f503adf2f24bcbf5430cc427fca5ee1f485b8
JangDongKuk <dkjang@naver.com>
2020-08-25 20:10:00 +0900

    서버가 보내준 응답을 메인화면 확인

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -2,8 +2,10 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
+import org.json.JSONObject
 
 //1:Baseactivity(BaseActivity.kt) -> 2:로그인화면(activity_main.xml)
 class MainActivity : BaseActivity() {
@@ -22,11 +24,18 @@ class MainActivity : BaseActivity() {
         //5:로그인버튼 -> 6:
         loginBtn.setOnClickListener {
 
-            val inputId =  idEdt.text.toString()
+            val inputId = idEdt.text.toString()
             val inputPw = pwEdt.text.toString()
 
             //5-1:API통신(파라메터 서버에 보내기)
-            ServerUtil.postRequestLogin(inputId, inputPw)
+            ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHeader {
+                override fun onResponse(json: JSONObject) {
+                    //실제 서버응답 실행
+                    Log.d("MainActivity(서버응답본문)", json.toString())
+
+                }
+
+            })
 
         }
 
--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
@@ -8,13 +8,19 @@ import java.io.IOException
 //3:OkHttp(Manifest+build.gradle) -> 4:postRequestLogin함수만들기
 class ServerUtil {
 
+    //5-2
+    //서버응답에 대한 실행할 인터페이스
+    interface JsonResponseHeader {
+        fun onResponse(json: JSONObject)
+    }
+
     //변수+함수 객체를 이용하지않고 클래스 기능으로 활용
     companion object {
 
         val BASE_URL = "http://15.164.153.174" //호스트주소
 
         //4:postRequestLogin함수만들기 -> 5:로그인버튼
-        fun postRequestLogin(id: String, pw: String) {
+        fun postRequestLogin(id: String, pw: String, handler: JsonResponseHeader?) {
             val client = OkHttpClient() //클라언트동작
             val urlStr = "${BASE_URL}/user" //주소완성
             //파라미터(POST/PUT/PATCH) 값
@@ -41,9 +47,11 @@ class ServerUtil {
                     val bodyString = response.body!!.string()
                     //String -> jSON Object 변환
                     val json = JSONObject(bodyString)
-                    Log.d("서버응답본문", json.toString())
+                    Log.d("ServerUtil(서버응답본문)", json.toString())
                     //{"code":400,"message":"존재하지 않는 이메일입니다."}
                     //{"code":400,"message":"비밀번호가 틀립니다."}
+                    //if (handler != null) handler.onResponse(json)
+                    handler?.onResponse(json)
 
                 }
 
