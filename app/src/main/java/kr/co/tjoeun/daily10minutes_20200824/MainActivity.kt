df2b17102d171d6df90b3d33cafea2b2d3af5bb4
JangDongKuk <dkjang@naver.com>
2020-08-25 19:55:29 +0900

    로그인시도 기능완성 -서버응답확인

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.daily10minutes_20200824
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
 
 //1:Baseactivity(BaseActivity.kt) -> 2:로그인화면(activity_main.xml)
 class MainActivity : BaseActivity() {
@@ -25,7 +26,7 @@ class MainActivity : BaseActivity() {
             val inputPw = pwEdt.text.toString()
 
             //5-1:API통신(파라메터 서버에 보내기)
-
+            ServerUtil.postRequestLogin(inputId, inputPw)
 
         }
 
--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ServerUtil.kt
@@ -1,6 +1,8 @@
 package kr.co.tjoeun.daily10minutes_20200824.utils
 
+import android.util.Log
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
 
 //3:OkHttp(Manifest+build.gradle) -> 4:postRequestLogin함수만들기
@@ -12,7 +14,7 @@ class ServerUtil {
         val BASE_URL = "http://15.164.153.174" //호스트주소
 
         //4:postRequestLogin함수만들기 -> 5:로그인버튼
-        fun postRequestLogin(id : String, pw: String){
+        fun postRequestLogin(id: String, pw: String) {
             val client = OkHttpClient() //클라언트동작
             val urlStr = "${BASE_URL}/user" //주소완성
             //파라미터(POST/PUT/PATCH) 값
@@ -26,13 +28,22 @@ class ServerUtil {
                 .post(formData)
                 .build()
 
-            client.newCall(request).enqueue(object :Callback{
+            client.newCall(request).enqueue(object : Callback {
                 //서버연결실패할경우
                 override fun onFailure(call: Call, e: IOException) {
 
                 }
+
                 //서버연결성공할경우
                 override fun onResponse(call: Call, response: Response) {
+
+                    //서버가 보내준 본문
+                    val bodyString = response.body!!.string()
+                    //String -> jSON Object 변환
+                    val json = JSONObject(bodyString)
+                    Log.d("서버응답본문", json.toString())
+                    //{"code":400,"message":"존재하지 않는 이메일입니다."}
+                    //{"code":400,"message":"비밀번호가 틀립니다."}
 
                 }
 
