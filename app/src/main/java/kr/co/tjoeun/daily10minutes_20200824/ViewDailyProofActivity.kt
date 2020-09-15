8d770d4971348d256120d917f31e4cf50667d041
dkjang <dkjang@naver.com>
2020-09-15 22:19:44 +0900

    날짜선택번튼을 누름면 DatePickerDialog 출현

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewDailyProofActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewDailyProofActivity.kt
@@ -1,9 +1,15 @@
 package kr.co.tjoeun.daily10minutes_20200824
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_view_daily_proof.*
 
 class ViewDailyProofActivity : BaseActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_daily_proof)
@@ -11,12 +17,22 @@ class ViewDailyProofActivity : BaseActivity() {
         setValues()
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun setupEvents() {
-        TODO("Not yet implemented")
+
+        selectDateBtn.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(mContext, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                //날짜가 선택되면 실행해줄 코드
+                Log.d("선택된월", month.toString())
+
+            }, 2020, 9 ,15)
+            datePickerDialog.show()
+        }
+
     }
 
     override fun setValues() {
-        TODO("Not yet implemented")
+
     }
 
 }
